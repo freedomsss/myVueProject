d4d145a8132d220104cf2730a15993378ee4c4ce
沈文文 <1546165212@qq.com>
2018-05-31 15:05:17 +0800

    初始上传

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <p>{{count}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <div class="box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  computed: {
+    count () {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    increment () {
+      this.$store.commit('increment')
+    },
+    decrement () {
+      this.$store.commit('decrement')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+  body {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .hello{
+    font-size: 20px;
+  }
+  .box{
+    outline: solid 2px;
+    max-width: 15em;
+    max-height: 10em;
+  }
+  @root:2em;
+  .box{
+    position: relative;
+    &::before{
+      content: '';
+      position: absolute;
+      padding: @root;
+      box-shadow: 0 0 7px #b53;
+      background-color: #95a;
+    }
+  }
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+  count: 0
+}
+
+export default new Vuex.Store({
+  state,
+  mutations: {
+    increment: state => state.count++,
+    decrement: state => state.count--
+  }
+})
